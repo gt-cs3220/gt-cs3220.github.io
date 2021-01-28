b9a22f42d44067856d768b6aa749ee4aa76dd9ba
HanningChen <hanningchen97@gmail.com>
2021-01-28 08:53:22 -0500

    get rid some unnecessary ports

--- assignment2_files/bubblesort.v
+++ assignment2_files/bubblesort.v
@@ -5,42 +5,63 @@ module bubblesort(
     clk,
     reset, 
     done, 
-    wr_adr,
-    wr_en,
-    dat_in,
     rd_en, 
-    rd_adr,
-    dat_out
-    );
+    dat_out0,
+    dat_out1,
+    dat_out2,
+    dat_out3,
+    dat_out4,
+    dat_out5,
+    dat_out6,
+    dat_out7,
+    dat_out8,
+    dat_out9
+);
     
     input clk;
     input reset;
-    input [15:0] dat_in;
-    input [9:0] wr_adr;
-    input wr_en;
     input rd_en;
-    output [15:0] dat_out;
+    output [15:0] dat_out0;
+    output [15:0] dat_out1;
+    output [15:0] dat_out2;
+    output [15:0] dat_out3;
+    output [15:0] dat_out4;
+    output [15:0] dat_out5;
+    output [15:0] dat_out6;
+    output [15:0] dat_out7;
+    output [15:0] dat_out8;
+    output [15:0] dat_out9;
     output done; 
-    input [9:0] rd_adr; 
  
-    parameter NumItm=10 ;
+    parameter NumItm = 10 ;
 
-    reg[15:0] dmem[9:0];   
+    reg[15:0] dmem [9:0];
+
+    assign dat_out0 = dmem[0];
+    assign dat_out1 = dmem[1];
+    assign dat_out2 = dmem[2];
+    assign dat_out3 = dmem[3];
+    assign dat_out4 = dmem[4];
+    assign dat_out5 = dmem[5];
+    assign dat_out6 = dmem[6];
+    assign dat_out7 = dmem[7];
+    assign dat_out8 = dmem[8];
+    assign dat_out9 = dmem[9];   
     
-    
+    /*
+        please consider dmem as memory from wich you will read values.
+    */
     initial begin
+        $readmemh("ex1.mem", dmem);
+    end
+  
 
-	 $readmemh("ex1.mem", dmem);
-     end
     
-
     
     // --------- Design implementation ----------
     // 
-    always @(posedge clk or posedge reset) 
-    begin
-   
+    always @(posedge clk or posedge reset) begin
+ 
     end // always 
     
-    endmodule 
-
+    endmodule
--- assignment2_files/test.v
+++ assignment2_files/test.v
@@ -3,9 +3,7 @@
 
 module test(
 
-    );
-
-
+);
 
     reg reset;
     reg sim_clk;
@@ -14,54 +12,118 @@ module test(
     
     parameter CLK_PERIOD = 10;  // 10 ns = 100 MHZ 
 
-    reg [15:0] dat_in;
-    reg [9:0] wr_adr;
-    reg wr_en;
     reg rd_en; 
     
-    wire [15:0] dat_out;
+    wire [15:0] dat_out0;
+    wire [15:0] dat_out1;
+    wire [15:0] dat_out2;
+    wire [15:0] dat_out3;
+    wire [15:0] dat_out4;
+    wire [15:0] dat_out5;
+    wire [15:0] dat_out6;
+    wire [15:0] dat_out7;
+    wire [15:0] dat_out8;
+    wire [15:0] dat_out9;
+
     wire done; 
-    reg [9:0] rd_adr; 
     
-    integer i; 
-    
-    
-    bubblesort bubblesort(.clk(sim_clk), .reset(reset), .dat_in(dat_in), .done(done),  .wr_adr(wr_adr), .wr_en(wr_en), .dat_out(dat_out), .rd_en(rd_en), .rd_adr(rd_adr)); 
-    
-// generate clock and reset 
+    //j is the reading address
+    integer i;
+     
+    bubblesort bubblesort(
+        .clk(sim_clk), 
+        .reset(reset),  
+        .done(done),  
+        .rd_en(rd_en), 
+        .dat_out0(dat_out0),
+        .dat_out1(dat_out1),
+        .dat_out2(dat_out2),
+        .dat_out3(dat_out3),
+        .dat_out4(dat_out4),
+        .dat_out5(dat_out5),
+        .dat_out6(dat_out6),
+        .dat_out7(dat_out7),
+        .dat_out8(dat_out8),
+        .dat_out9(dat_out9));
+
+
+//  generate clock and reset 
     initial sim_clk = 1'b0;
     
     always #(CLK_PERIOD/2.0)
         sim_clk = ~sim_clk; 
      
-     initial begin 
-      reset = 1'b1; 
-      i = 0; 
-       rd_en = 0; 
-       wr_en = 0; 
-       rd_adr = 0; 
-     // reset goes inactive after 20 clocks 
-     
-     #20; 
-        reset = 0; 
-      
-      end
-    
-     always @(posedge sim_clk) 
-        begin 
-        i = i + 1; 
-        if (i > 896)  
-            begin 
-            rd_en <= 1; 
-            rd_adr <= i[3:0];
-         end 
-         
-        $monitor(sim_clk, " sim_clk=%b, done:%b rd_adr:%h wr_adr:%h dat_in:%h dat_out:%h wr_en:%b rd_en:%b", sim_clk, done, rd_adr, wr_adr, dat_in, dat_out, wr_en, rd_en); 
-        
-       
+    initial begin 
+        reset = 1'b1; 
+        i = 0;
+        rd_en = 0;  
+//  reset goes inactive after 20 clocks 
+        #20; 
+        reset = 0;
+        #280
+        $display("Bubblesort test start");
+        #480
+        if(dat_out9 != 'h500) begin
+            $display("Something wrong happened");
+            $finish;
         end
-  
-  
-  endmodule 
-        
-         
+        #420
+        if(dat_out8 != 'h300) begin
+            $display("Something wrong happened");
+            $finish;
+        end
+        #360
+        if(dat_out7 != 'h100) begin
+            $display("Something wrong happened");
+            $finish;
+        end
+        #360
+        if(dat_out6 != 'h50) begin
+            $display("Something wrong happened");
+            $finish;
+        end
+        #360
+        if(dat_out5 != 'h50) begin
+            $display("Something wrong happened");
+            $finish;
+        end
+        #360
+        if(dat_out4 != 'h10) begin
+            $display("Something wrong happened");
+            $finish;
+        end
+        #360
+        if(dat_out3 != 'h10) begin
+            $display("Something wrong happened");
+            $finish;
+        end
+        #360
+        if(dat_out2 != 'h3) begin
+            $display("Something wrong happened");
+            $finish;
+        end
+        #360
+        if(dat_out1 != 'h2) begin
+            $display("Something wrong happened");
+            $finish;
+        end
+        #360
+        if(dat_out0 != 'h1) begin
+            $display("Something wrong happened");
+            $finish;
+        end
+        #10
+        $display("Pass all testcases, congratulation");
+        $finish;
+    end
+    
+    always @(posedge sim_clk) begin 
+        i = i + 1; 
+        if (i > 29) begin 
+            rd_en <= 1; 
+        end
+        //You could uncommnet this line for debugging  
+        //$monitor(sim_clk, " sim_clk=%b, done:%b rd_en:%b dat_out0:%h dat_out1:%h dat_out2:%h dat_out3:%h dat_out4:%h dat_out5:%h dat_out6:%h dat_out7:%h dat_out8:%h dat_out9:%h", sim_clk, done, rd_en, dat_out0, dat_out1, dat_out2, dat_out3, dat_out4, dat_out5, dat_out6, dat_out7, dat_out8, dat_out9);  
+    end
+
+endmodule
