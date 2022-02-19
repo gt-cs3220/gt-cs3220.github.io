9b7142443baa39b1133dc019c834455bd1da63d5
Anurag Kar <anuragkar09@gmail.com>
2022-02-19 15:07:57 -0500

    Added grepping for total passed/failed tests in run_tests.sh

--- project2_files/run_tests.sh
+++ project2_files/run_tests.sh
@@ -25,6 +25,12 @@ case $part in
 			IDMEMINITFILE=$filename make tests>> part1_tests.log
 		done
 		grep -E 'TESTING|Failed|Passed' part1_tests.log>part1_results.log
+		echo "Total number of tests:"
+		grep -i testing part1_tests.log |wc -l
+		echo "Number of passed tests:"
+		grep -i passed part1_tests.log |wc -l
+		echo "Number of failed tests:"
+		grep -i failed part1_tests.log |wc -l
 	;;
 	part2)
 		echo RUNNING PART2$'\n'
@@ -33,6 +39,12 @@ case $part in
 			IDMEMINITFILE=$filename make tests>> part2_tests.log
 		done
 		grep -E 'TESTING|Failed|Passed' part2_tests.log>part2_results.log
+		echo "Total number of tests:"
+		grep -i testing part2_tests.log |wc -l
+		echo "Number of passed tests:"
+		grep -i passed part2_tests.log |wc -l
+		echo "Number of failed tests:"
+		grep -i failed part2_tests.log |wc -l
 	;;
 	part3)
 		echo RUNNING PART3$'\n'
@@ -41,6 +53,12 @@ case $part in
 			IDMEMINITFILE=$filename make tests>> part3_tests.log
 		done
 		grep -E 'TESTING|Failed|Passed' part3_tests.log>part3_results.log
+		echo "Total number of tests:"
+		grep -i testing part3_tests.log |wc -l
+		echo "Number of passed tests:"
+		grep -i passed part3_tests.log |wc -l
+		echo "Number of failed tests:"
+		grep -i failed part3_tests.log |wc -l
 	;;
 	all)
 		echo RUNNING ALL$'\n'
@@ -49,6 +67,12 @@ case $part in
 			IDMEMINITFILE=$filename make tests>> all_tests.log
 		done
 		grep -E 'TESTING|Failed|Passed' all_tests.log>all_results.log
+		echo "Total number of tests:"
+		grep -i testing all_tests.log |wc -l
+		echo "Number of passed tests:"
+		grep -i passed all_tests.log |wc -l
+		echo "Number of failed tests:"
+		grep -i failed all_tests.log |wc -l
 	;;
 	*)
 		echo INVALID PART. Use part1, part2, part2 or all as command line argument
